f1af69aaabb58b7c2f22e635beb74bc2b41ae22d
Daniel Nguyen <mduongbkhn@gmail.com>
2023-04-19 17:15:19 +0900

    Update manager.py

--- manager.py
+++ manager.py
@@ -460,8 +460,9 @@ class Training:
             - Search over rewards[agent] - if rewards = 1 -> removes all other rewards/obs/act/log_probs/obs_val/term
             """
             if self.args.selective_buffer:
+                print(args.selective_buffer)
                 for agent in self.agent_names:
-                    print(rewards)
+                    print(agent)
                     selective_mask = mask_checkout(rewards[agent])
                     tmp_obs, tmp_act, tmp_probs, \
                     tmp_rew, tmp_obs_val, tmp_term = select_obs(obs = curr_obs,
